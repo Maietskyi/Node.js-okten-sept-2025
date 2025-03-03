2e4ca776f059b101b401bf6a1b55a0ca6e24d874
Maietskyi <damaietskyi@gmail.com>
2025-03-03 22:17:14 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log('Hello from NodeJs');
+//
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+//
+// const {a, myFunc} = require ('./services/test');
+// console.log(a);
+// myFunc();
+
+// Стандартні імпорти ноди
+
+
+
+
+
---
+++ services/test.js
@@ -0,0 +1,17 @@
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+//
+// const a = 5;
+//
+// const myFunc = ()=>{
+//     console.log('test/myFunc');
+// }
+//
+// module.exports = {
+//     a,
+//     myFunc
+// }
+
+
+
