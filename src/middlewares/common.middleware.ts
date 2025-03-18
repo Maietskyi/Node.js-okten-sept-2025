b6f5d5967f61491b4252db336c4c183c41873f1b
Maietskyi <damaietskyi@gmail.com>
2025-03-18 11:42:23 +0200

    Lesson 4

---
+++ src/middlewares/common.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { isObjectIdOrHexString } from "mongoose";
+import { ApiError } from "../errors/api.error";
+import { ObjectSchema } from "joi";
+
+class CommonMiddleware {
+    public isIdValidate(key: string) {
+        return (req: Request, res: Response, next: NextFunction): any => {
+            try {
+                const { id } = req.params;
+                if (!isObjectIdOrHexString(id)) {
+                    throw new ApiError(`Invalide Id [${key}]`, 400);
+                }
+            } catch (e) {
+                next(new ApiError(e.deletes[0].message, 400));
+            }
+        };
+    }
+    public validateBody(validator: ObjectSchema): any {
+        return async (req: Request, res: Response, next: NextFunction) => {
+            try {
+                req.body = await validator.validateAsync(req.body);
+            } catch (e) {
+                next(e);
+            }
+        };
+    }
+}
+export const commonMiddleware = new CommonMiddleware();
