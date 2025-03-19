4c6a182436928809eccb82f6709eac93f63d1124
Maietskyi <damaietskyi@gmail.com>
2025-03-20 00:23:16 +0200

    Lesson 5

---
+++ src/interfaces/token.interface.ts
@@ -0,0 +1,22 @@
+import { IBase } from "./base.interface";
+import { RoleEnum } from "../enums/role.enum";
+
+interface IToken extends IBase {
+    _id: string;
+    accessToken: string;
+    refreshToken: string;
+    _userId: string;
+}
+
+interface ITokenPayload {
+    userId: string;
+    role: RoleEnum;
+}
+
+type ITokenPair = Pick<IToken, "accessToken" | "refreshToken">;
+
+export {
+    IToken,
+    ITokenPayload,
+    ITokenPair,
+};
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,23 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
+import { RoleEnum } from "../enums/role.enum";
+import { string } from "joi";
 
 const userSchema = new Schema(
     {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: {
+            enum: RoleEnum,
+            type: string,
+            required: true,
+            default: RoleEnum.USER,
+        },
         name: { type: String, required: true },
         surname: { type: String, required: true },
         age: { type: Number, required: true },
+        isDeleted: { type: Boolean, default: false },
+        isVerified: { type: Boolean, default: false },
     },
     { timestamps: true, versionKey: false },
 );
