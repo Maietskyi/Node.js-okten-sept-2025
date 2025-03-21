dbe1f88896ff2af61be0c5df99d03fed239d3ecd
Maietskyi <damaietskyi@gmail.com>
2025-03-21 16:37:12 +0200

    Lesson 5-1 working code

---
+++ src/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { IUser, IUserCreateDTO } from "../interfaces/user.interface";
+import { ITokenPair } from "../interfaces/token.interface";
+import { userService } from "./user.service";
+import { passwordService } from "./password.service";
+import { userRepository } from "../repositories/user.repository";
+import { tokenService } from "./token.service";
+import { tokenRepository } from "../repositories/token.repository";
+import { ApiError } from "../errors/api.error";
+import { StatusCodeEnum } from "../enums/status-codes";
+
+class AuthService {
+    public async signUp(user: IUserCreateDTO): Promise<{ user: IUser, tokens: ITokenPair }> {
+        await userService.isEmailUnique(user.email);
+        const password = await passwordService.hashedPassword(user.password);
+        const newUser = await userRepository.create({ ...user, password });
+        const tokens = tokenService.generateTokens({
+            userId: newUser._id,
+            role: newUser.role,
+        });
+        await tokenRepository.create({ ...tokens, _userId: newUser._id });
+        return { user: newUser, tokens };
+    }
+
+    public async signIr(dto: any): Promise<{ user: IUser, tokens: ITokenPair }> {
+        const user = await userRepository.getByEmail(dto.email);
+        const isValidPassword = await passwordService.comparePassword(dto.password, user.password);
+        if (!isValidPassword) {
+            throw new ApiError("Invalid email or password", StatusCodeEnum.UNAUTHORIZED);
+        }
+        const tokens = tokenService.generateTokens({
+            userId: user._id,
+            role: user.role,
+        });
+        await tokenRepository.create({ ...tokens, userId: user._id });
+        return { user, tokens };
+    }
+}
+
+export const authService = new AuthService();
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,23 +12,35 @@ class UserService {
         return userRepository.create(user);
     }
 
-    public getById(userId: string): Promise<IUser> {
-        return userRepository.getById(userId);
+    public async getById(userId: string): Promise<IUser> {
+        const user = await userRepository.getById(userId);
+        if (!user) {
+            throw new ApiError("User not found", StatusCodeEnum.NOT_FOUND);
+        }
+        return user;
     }
 
-    public updateById(userId: string, user: IUserUpdateDTO): Promise<IUser> {
-        return userRepository.updateById(userId, user);
+    public async updateById(userId: string, user: IUserUpdateDTO): Promise<IUser> {
+        const data = await userRepository.getById(userId);
+        if (!data) {
+            throw new ApiError("User not found", StatusCodeEnum.NOT_FOUND);
+        }
+        return await userRepository.updateById(userId, user);
     }
 
-    public deleteById(userId: string): Promise<IUser> {
-        return userRepository.deleteById(userId);
+    public async deleteById(userId: string): Promise<IUser> {
+        const data = await userRepository.getById(userId);
+        if (!data) {
+            throw new ApiError("User not found", StatusCodeEnum.NOT_FOUND);
+        }
+        return await userRepository.deleteById(userId);
     }
 
     public async isEmailUnique(email: string): Promise<void> {
-    const user = await userRepository.getByEmail(email);
-    if (user) {
-        throw new ApiError("User is already exist", StatusCodeEnum.BED_REQUEST)
-    }
+        const user = await userRepository.getByEmail(email);
+        if (user) {
+            throw new ApiError("User is already exist", StatusCodeEnum.BED_REQUEST);
+        }
     }
 }
 
